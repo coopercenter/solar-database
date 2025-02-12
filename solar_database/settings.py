77923b86ff3ff7f071e85c0d2d31092b40d10871
jw4ng <jadenwang19@gmail.com>
2025-02-12 11:19:56 -0500

    Changed DEBUG to TRUE

--- solar_database/settings.py
+++ solar_database/settings.py
@@ -25,10 +25,10 @@ SECRET_KEY = 'django-insecure-#kru)))h+3s2lr4h)p_sbj!#v+o$d0v)vycw#@3#&%7dj^(1@&
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-if os.path.exists('hidden'):
-    DEBUG = True
-else:
-    DEBUG = False
+# if os.path.exists('hidden'):
+DEBUG = True
+#else:
+    # DEBUG = False
 
 ALLOWED_HOSTS = ['va-solar-db.azurewebsites.net', '127.0.0.1', 'solardatabase.coopercenter.org']
 
