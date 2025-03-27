c21c46dfa59dd0acbcf7f9343bfa6057ffb14c99
EmilyLien <egl6a@virginia.edu>
2025-03-27 13:37:50 -0400

    Adding dictionary page to views

--- database/views.py
+++ database/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import csv
-from .models import SolarProjectData
+from .models import SolarProjectData, DataDictionary
 from django.views.generic import DetailView
 from .plotly_dash import dashapp
 from django_plotly_dash import DjangoDash
@@ -30,6 +30,25 @@ def export_csv(request):
 
     return response
 
+def export_dictionary_csv(request):
+    response = HttpResponse(
+        content_type="text/csv",
+        headers={"Content-Disposition": 'attachment; filename="vasolardatadictionary.csv"'},
+    )
+
+    data = DataDictionary.objects.all()
+
+    field_names = [field.name for field in DataDictionary._meta.fields]
+
+    writer = csv.DictWriter(response, fieldnames=field_names)
+    writer.writeheader()
+
+    for obj in data:
+        row = {field: getattr(obj, field) for field in field_names}
+        writer.writerow(row)
+
+    return response
+
 def dash(request):
     return render(request, 'database/dash.html',)
 
@@ -47,6 +66,13 @@ def data(request):
     }
 
     return render(request, 'database/data.html', context)
+
+def dictionary(request):
+    
+    datadictionary = DataDictionary.objects.all()
+    context = {'datadictionary': datadictionary}
+
+    return render(request, 'database/dictionary.html',context)
 
 class ProjectView(DetailView):
     model = SolarProjectData
