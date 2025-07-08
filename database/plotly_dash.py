0268a246983d61445c8cd47dfd7081b61334f8df
EmilyLien <egl6a@virginia.edu>
2025-07-08 12:05:26 -0400

    Table sorting fix, new graph, debug off, add main yaml for workflow improvement

--- database/plotly_dash.py
+++ database/plotly_dash.py
@@ -52,8 +52,15 @@ annualData = pd.merge(annualData,annualTotal,on='final_action_year')
 annualData['annual_rate'] = round(annualData['project_count']/annualData['annual_total'],2)
 #define the regional data, straightforward summary of relevant datapoints by region. More could be added
 regionalData = pd.DataFrame(df.groupby(['region','local_permit_status']).agg({'project_mw':'sum','data_id':'count','public_project_acres':'sum'}).reset_index())
+
 #define the size category data, summarizing by category
-sizeCategoryData = pd.DataFrame(df.groupby(["mw_size_range",'local_permit_status']).agg({'project_mw':'sum','data_id':'count'}).reset_index())
+sizeCategoryData = pd.DataFrame(df.groupby(["mw_size_range",'local_permit_status']).agg({'project_mw':'sum','data_id':'count'}).reset_index()).rename(columns={'data_id':'project_count'})
+#get the project and megawatt totals by size category
+size_summary = sizeCategoryData.groupby('mw_size_range').agg({'project_mw':'sum','project_count':'sum'}).reset_index().rename(columns={'project_mw':'size_total_mw','project_count':'size_total_projects'})
+sizeCategoryData = pd.merge(sizeCategoryData,size_summary,how='left',on='mw_size_range')
+#calculate the percent of each size category per local permit status (i.e, out of all 150MW projects, what percent have been Approved/Denied/etc.)
+sizeCategoryData['percent_of_size_mw'] = sizeCategoryData['project_mw']/sizeCategoryData['size_total_mw']
+sizeCategoryData['percent_of_size_projects'] = sizeCategoryData['project_count']/sizeCategoryData['size_total_projects']
 
 #get the county fips crosswalk for mapping
 countyData = CountyData.objects.values('locality',
@@ -417,7 +424,7 @@ mwRegionalBar.update_layout(margin=dict(l=5, r=5, t=60, b=0),
                                        tickmode='array',
                                        tickvals=['Southern','Central','Hampton Roads','Valley','Northern','Eastern','West Central','Southwest'],title=''),
                             yaxis=dict(tickformat=",.0f",
-                                       title="Total Megawatts"))
+                                       title="Megawatts"))
      
 mwRegionalBar.add_annotation(text='<i>Source: Weldon Cooper Center <br> Virginia Solar Database</i>',x=0,y=-.17,xref="paper", yref="paper",font=dict(size=8),showarrow=False)
 
@@ -425,7 +432,7 @@ sizeMWBar = px.bar(sizeCategoryData[sizeCategoryData['local_permit_status'].isin
                    x="mw_size_range", 
                    y="project_mw",
                    color='local_permit_status',
-                   custom_data=['local_permit_status','data_id'],
+                   custom_data=['local_permit_status','project_count'],
                    height=550,
                    width=480,
                    color_discrete_sequence=['rgb(40, 67, 118)', 
@@ -455,13 +462,13 @@ sizeMWBar.update_layout(margin=dict(l=5, r=5, t=25, b=5),
                                    tickvals=['≤5MW','5MW< - ≤20MW','20MW< - ≤150MW','150MW<'],
                                    title=''),
                         yaxis=dict(tickformat=",.0f",
-                                   title='Total Megawatts'))
+                                   title='Megawatts'))
 
 sizeMWBar.add_annotation(text='<i>Source: Weldon Cooper Center Virginia Solar Database</i>',x=0,y=-.07,xref="paper", yref="paper",font=dict(size=8),showarrow=False)
 
 sizeProjectsBar = px.bar(sizeCategoryData[sizeCategoryData['local_permit_status'].isin(["Approved","Denied",'Withdrawn'])], 
                          x="mw_size_range", 
-                         y='data_id',
+                         y='project_count',
                          color='local_permit_status',
                          custom_data=['local_permit_status','project_mw'],
                          height=550,
@@ -469,7 +476,7 @@ sizeProjectsBar = px.bar(sizeCategoryData[sizeCategoryData['local_permit_status'
                          color_discrete_sequence=['rgb(40, 67, 118)', 
                                                   'rgb(229, 114, 0)',
                                                   'rgb(200, 203, 210)'],
-                         labels={'data_id':'Project Count',
+                         labels={'project_count':'Project Count',
                                  'mw_size_range':'Megawatts',
                                  'local_permit_status':'Local Permit Status'},
                          barmode='group',
@@ -491,12 +498,57 @@ sizeProjectsBar.update_layout(margin=dict(l=5, r=5, t=25, b=5),
                               font=dict(size=10,
                                         color='#242e4c'),
                               #title_subtitle=dict(text='Source: Weldon Cooper Center for Public Service', font=dict(size=15)),
-                              yaxis=dict(title="Total Projects"),
+                              yaxis=dict(title="Projects"),
                               xaxis=dict(tickmode='array',
                                          tickvals=['≤5MW','5MW< - ≤20MW','20MW< - ≤150MW','150MW<'],
                                          title=''))
 
 sizeProjectsBar.add_annotation(text='<i>Source: Weldon Cooper Center Virginia Solar Database</i>',x=0,y=-.07,xref="paper", yref="paper",font=dict(size=8),showarrow=False)
+
+#graphing the percent of projects in each size category by decision category
+sizePercentBar = px.bar(sizeCategoryData[(sizeCategoryData.local_permit_status != 'Pending') & (sizeCategoryData.local_permit_status != 'NA')],
+                        color='mw_size_range',
+                        custom_data=['project_count'],
+                        #text_auto=True,
+                        color_discrete_sequence=['rgb(40, 67, 118)', 
+                                      #'rgb(253, 218, 36)', 
+                                      'rgb(229, 114, 0)',
+                                      'rgb(200, 203, 210)',
+                                      'rgb(37, 202, 211)',
+                                      'rgb(98, 187, 70)'],
+             category_orders={"local_permit_status": ["Approved", 
+                                                      "Approved/Amended", 
+                                                      "Denied", 
+                                                      "Withdrawn", 
+                                                      "By-right"]},
+                        labels={'mw_size_range':'Project Size',
+                                'local_permit_status':'Local Permit Status',
+                                'percent_of_size_projects':'Percent of Projects in Size Range',
+                                'project_count':"Project Count"
+                                },
+                        x='local_permit_status',
+                        y='percent_of_size_projects',
+                        height=550,
+                        width=480,
+                        barmode='group')
+
+sizePercentBar.update_layout(margin=dict(l=5, r=5, t=25, b=5),
+                                paper_bgcolor='#F2F4F8',
+                                plot_bgcolor='#F2F4F8',
+                                yaxis=dict(tickformat='.0%'),
+                                xaxis=dict(title=''),
+                                font_family='franklin-gothic-urw-cond, sans-serif',
+                                font=dict(size=10,
+                                  color='#242e4c'),
+                      title=dict(font=dict(size=35)),
+                     legend=dict(font=dict(size=10,
+                                              color='#242e4c'),
+                                    orientation='h',
+                                    yanchor="bottom",
+                                    y=1,
+                                    x=-.13,
+                                    title=''))
+sizePercentBar.add_annotation(text='<i>Source: Weldon Cooper Center Virginia Solar Database</i>',x=0,y=-.07,xref="paper", yref="paper",font=dict(size=8),showarrow=False)
 
 dashapp = DjangoDash(name='SolarDash',add_bootstrap_links=True, external_stylesheets=[dbc.themes.FLATLY]) #,external_stylesheets=[dbc.themes.FLATLY] #putting this here limits it to dashboard
 
@@ -598,7 +650,9 @@ dashapp.layout =  dbc.Container([
                 labelClassName="btn btn--blue-d",
                 options=[
                 {"label": "Megawatts", "value": 'sizeMWBar'}, 
-                {"label": "Projects", "value": 'sizeProjectsBar'}
+                {"label": "Projects", "value": 'sizeProjectsBar'},
+                {"label": "Percent of Size", "value": 'sizePercentBar'}
+                
             ],
             value='sizeMWBar'
             ),
@@ -930,6 +984,8 @@ def update_size_chart(value):
         return sizeMWBar
     elif value=='sizeProjectsBar':
         return sizeProjectsBar
+    elif value=='sizePercentBar':
+        return sizePercentBar
     
 if __name__ == '__main__':
     dashapp.run_server(host='0.0.0.0', port=int(os.environ.get('PORT', 8050)))
