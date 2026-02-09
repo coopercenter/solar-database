68ae8e5d7cc2009dc7ce7afbed7f7e4b3fb59580
ahn5nc <ahn5nc@virginia.edu>
2026-02-09 16:22:12 -0500

    changed debug for dev test

--- solar_database/settings.py
+++ solar_database/settings.py
@@ -26,9 +26,9 @@ SECRET_KEY = 'django-insecure-#kru)))h+3s2lr4h)p_sbj!#v+o$d0v)vycw#@3#&%7dj^(1@&
 # SECURITY WARNING: don't run with debug turned on in production!
 
 if os.path.exists('hidden'):
-    DEBUG = False
-else:
     DEBUG = True
+else:
+    DEBUG = False
 
 ALLOWED_HOSTS = ['va-solar-db.azurewebsites.net','va-solar-db-dev.azurewebsites.net', '127.0.0.1', 'solardatabase.coopercenter.org']
 
